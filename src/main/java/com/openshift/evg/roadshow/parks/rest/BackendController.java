2e3fc14f3d661999d9e9103d8e91d340c3919b8a
ssafakk <100931211+ssafakk@users.noreply.github.com>
2022-03-04 14:03:15 +0300

    Update BackendController.java

--- src/main/java/com/openshift/evg/roadshow/parks/rest/BackendController.java
+++ src/main/java/com/openshift/evg/roadshow/parks/rest/BackendController.java
@@ -16,7 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class BackendController{
 
     @RequestMapping(method = RequestMethod.GET, produces = "application/json")
-    public Backend get() {
-        return new Backend("nationalparks","National Parks", new Coordinates("47.039304", "14.505178"), 4);
+    public Backend get() {        
+        return new Backend("nationalparks","Awesome National Parks", new Coordinates("47.039304", "14.505178"), 4);
     }
 }
